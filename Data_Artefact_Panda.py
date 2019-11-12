a6f27de373bc900e8a9229db03c3033d0d7c744e
Antoine-marchais <antoine.marchais@student.ecp.fr>
2019-11-12 15:16:37 +0100

    translations in english

--- Data_Artefact_Panda.py
+++ Data_Artefact_Panda.py
@@ -17,15 +17,22 @@
 # %% [markdown]
 # # Data Artefact
 
-# %%
-# !pip3 install pandas
+# %% [markdown]
+# ## Imports
 
 # %%
+import numpy as np 
 import pandas as pd
+import os
+import json
+import matplotlib.pyplot as plt
+
+# %% [markdown]
+# ## Lecture des articles
 
 # %%
-# On met tous les articles dans un Dataframe Panda, en y ajoutant l'année et le jour et la taille
-path = "Downloads/cleaned_articles"
+# putting all articles in a Dataframe, adding year, day, and size
+path = "./cleaned_articles"
 
 import os
 import json
@@ -33,70 +40,68 @@ all_json_documents = []
 all_articles = []
 
 years = os.listdir(path)
-del years[0]  #le premier dossier n'est pas une année
 
 for year in years:
     for filename in os.listdir(path + "/" + year):
-        f = open("Downloads/cleaned_articles/" + year + "/" + filename, 'r')
+        f = open("./cleaned_articles/" + year + "/" + filename, 'r')
         json_document = f.read()
         json_document = json.loads(json_document)
         all_json_documents.append(json_document)
         for article in json_document:
             article['year'] = year
-            article['date'] = filename[:len(filename) - 5] #retirer le .json
+            article['date'] = filename[:len(filename) - 5] #stripping the .json extension
             article['size'] = len(article['text'])
             all_articles.append(article)
 
 articles_df = pd.DataFrame(all_articles, columns=["year", "date", "title", "text", 'url', 'size'])
-articles_df[:3]
+articles_df['size'].describe()
 
 # %%
-import matplotlib.pyplot as plt
-
-# Nombre d'articles en tout
+# number of articles in the complete set
 nb_articles = len(articles_df.index)
-print("Nombres d'articles : ", nb_articles, '\n')
+print("# articles : ", nb_articles, '\n')
 
-# Nombre d'articles en fonction des années
+# number of articles per year
 years = articles_df['year']
 years = list(map(int, years))
 nb_years = articles_df['year'].value_counts().count()
 plt.hist(years, bins=nb_years)
-plt.gca().set(title="Nombre d'articles en fonction des années \n")
+plt.gca().set(title="# article per year \n")
 plt.show()
 
 
+# %% [markdown]
+# **Note**: On illustre ici le fait que les articles sont très peu répartis dans le temps, il serait peut être nécessaire d'équilibrer le dataset en cherchant des articles plus anciens.
+
 # %%
-# Articles ou citations ? On affiche la taille des articles
+# displaying article size
 
 plt.hist(articles_df['size'], bins=50)
-plt.gca().set(title='Répartition des articles en fonction de leur taille')
-plt.xlabel('Taille des articles en nb de caractères')
-plt.ylabel('Nombre d\articles')
+plt.gca().set(title='distribution of articles according to size')
+plt.xlabel('# of caracters per article')
+plt.ylabel('# of articles')
 plt.show()
 
 
 
 # %% [markdown]
-# Il y a donc beaucoup d'articles avec peu de caractères, il y a dedans des citations ou des articles tronqués
+# **Note:** Il y a donc beaucoup d'articles avec peu de caractères, en isolant les articles avec peu de caractères, on se rend compte qu'il s'agit majoritairement de citations et d'articles tronqués.
 
 # %%
-# Afficher les titres des articles avec moins de 400 caractères
+# Displaying articles with less than 400 caracters
 
 articles_df[articles_df['size'] < 400][['title', 'size', 'url']]
 
 
 # %%
-
-# quelques métriques
-print("Quelques métriques: \n")
+print("Some metrics on short articles: \n")
 nb_less_400 = len(articles_df[articles_df['size'] < 400])
-print("Poucentage d'articles à moins de 400 caractères : ", int(nb_less_400*100/nb_articles), "%")
+print("proportion of articles with less than 400 caracters : ", int(nb_less_400*100/nb_articles), "%")
 
 nb_less_600 = len(articles_df[articles_df['size'] < 600])
-print("Poucentage d'articles à moins de 600 caractères : ", int(nb_less_600*100/nb_articles), '%')
+print("proportion of articles with less than 600 caracters : ", int(nb_less_600*100/nb_articles), '%')
 
 nb_less_800 = len(articles_df[articles_df['size'] < 800])
-print("Poucentage d'articles à moins de 800 caractères : ", int(nb_less_800*100/nb_articles), '%')
+print("proportion of articles with less than 800 caracters : ", int(nb_less_800*100/nb_articles), '%')
 
 # %%
--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Projet-Artefact-actualité
 
-Le projet de Rétroéclairage de l'actualité est un projet étudiant réalisé en collaboration avec l'entreprise Artefact, 
-qui vise à mettre en place un chatbot archiviste. Ce bot sera capable de proposer à l'utilisateur des articles d'archive
-mis en relation avec des articles d'actualité grâce à un algorithme de matching exploitant les dernières technologies
-du Traitement de Langage Naturel (ou NLP pour Natural Language Processing en anglais).
+This project is a student project backed by the french data consulting company Artefact, aiming at the creation of an
+archivist bot. This bot will be able to suggest archive articles related to recent news to the end user, based on a
+matching algorithm leveraging the latest technologies in Natural Language processing.
 
-## Prérequis: 
-- afin de faciliter l'échange de notebook pour le machine learning, nous utilisons le module jupytext qui permet de versionner
-des scripts python avant de les convertir en notebook en local. 
-installation : `pip install jupytext`
+## Prerequisites: 
+- We use the jupytext module to simplify version control of jupyter notebooks, which allows easy conversions between notebooks
+and python files.
+install : `pip install jupytext`
