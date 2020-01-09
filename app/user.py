9c283446ee1e9852e4fa6ae66b0c00606b0e00b8
Simon Cloître <simon.cloitre@hotmail.fr>
2020-01-09 11:29:18 +0100

    cleaning with actions classes

--- app/user.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    action = db.Column(db.String(64), index=True)
-    context = db.Column(db.String(64), index=True)
