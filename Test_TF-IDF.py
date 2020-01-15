cd445654d6289025e9ee3514a38bd7fae0f54bff
AnneLaurene <annelaurene.harmel@free.fr>
2020-01-15 16:31:05 +0100

    TF-IDF create matching algorithm

--- Test_TF-IDF.py
+++ Test_TF-IDF.py
@@ -28,6 +28,8 @@
 
 # %%
 import os
+from math import *
+
 import json
 from nltk import word_tokenize
 from stop_words import get_stop_words
@@ -40,32 +42,6 @@ from sklearn.feature_extraction.text import TfidfTransformer
 
 from nltk.stem.snowball import FrenchStemmer
 stemmer = FrenchStemmer()
-
-# %%
-articles = []
-titles = []
-print('...')
-for year in os.listdir('./cleaned_articles/'):
-    if year != '.DS_Store':
-        for filename in os.listdir('./cleaned_articles/'+year):
-            if (len(articles)>-1):
-                    path = './cleaned_articles/'+year+'/'+filename
-                    file = open(path, 'r')
-                    read_file = json.loads(file.read())[0]
-                    titles.append(read_file['title'])
-                    read_file = read_file['text']
-
-                    #removing punctuation, stop words and lemmatize
-                    text = "".join([char if (char.isalnum() or char==" ") else " " for char in read_file])
-                    text = word_tokenize(text)
-                    text = [word.lower() for word in text if word.lower() not in stop_words]
-                    text = [stemmer.stem(word) for word in text]
-                    text = [word for word in text if len(word)>3 and word not in stop_words]
-                    text = " ".join(text)
-
-                    articles.append(text)
-print('Done')
-#print(articles)
 
 # %% [markdown]
 # ### Creating list of stop-words
@@ -86,14 +62,47 @@ stop_words_to_add += get_stop_words('fr')
 for word in stop_words_to_add:
     stop_words.add(word)
 
+# %%
+articles = []
+processed_articles = []
+titles = []
+print('...')
+for year in os.listdir('./cleaned_articles/'):
+    if year != '.DS_Store':
+        for filename in os.listdir('./cleaned_articles/'+year):
+            if (len(articles)<5000):
+                    path = './cleaned_articles/'+year+'/'+filename
+                    file = open(path, 'r')
+                    read_file = json.loads(file.read())
+                    for article in read_file:
+                        title = article['title']
+                        article = article['text']
+
+                        #removing punctuation, stop words and lemmatize
+                        text = "".join([char if (char.isalnum() or char==" ") else " " for char in article])
+                        text = word_tokenize(text)
+                        text = [word.lower() for word in text if word.lower() not in stop_words]
+                        text = [stemmer.stem(word) for word in text]
+                        text = [word for word in text if len(word)>3 and word not in stop_words]
+                        text = " ".join(text)
+                        if len(text) > 200:
+                            processed_articles.append(text)
+                            articles.append(article)
+                            titles.append(title)
+                        if len(processed_articles)%100 == 0:
+                            print(len(processed_articles))
+print('Done')
+print(len(processed_articles))
+
+
 # %% [markdown]
 # ## Creating Vocabulary and words count
 
 # %%
 #create a vocabulary of words, 
-#ignore words that appear in 85% of documents, 
-cv=CountVectorizer(max_df=0.80, stop_words=stop_words, max_features=10000)
-word_count_vector=cv.fit_transform(articles)
+#ignore words that appear in 80% of documents, 
+cv=CountVectorizer(max_df=0.85, min_df=0, stop_words=stop_words, max_features=10000)
+word_count_vector=cv.fit_transform(processed_articles)
 
 # %%
 # display 10 firt words of our vocabulary
@@ -134,6 +143,18 @@ def extract_topn_from_vector(feature_names, sorted_items, topn=10):
     
     return results
 
+def get_kekwords_from_text(doc, nb_vectors):
+    #generate tf-idf for the given document
+    tf_idf_vector=tfidf_transformer.transform(cv.transform([doc]))
+    
+    #sort the tf-idf vectors by descending order of scores
+    sorted_items=sort_coo(tf_idf_vector.tocoo())
+    
+    #extract only the top n; n here is 10
+    keywords=extract_topn_from_vector(feature_names,sorted_items, nb_vectors)
+    
+    return keywords
+
 
 # %%
 # you only needs to do this once, this is a mapping of index to 
@@ -141,64 +162,110 @@ feature_names=cv.get_feature_names()
  
 # get the document that we want to extract keywords from
 articleNb=143
-doc=articles[articleNb]
+doc=processed_articles[articleNb]
 title=titles[articleNb]
  
-#generate tf-idf for the given document
-tf_idf_vector=tfidf_transformer.transform(cv.transform([doc]))
- 
-#sort the tf-idf vectors by descending order of scores
-sorted_items=sort_coo(tf_idf_vector.tocoo())
- 
-#extract only the top n; n here is 10
-keywords=extract_topn_from_vector(feature_names,sorted_items,10)
+keywords = get_kekwords_from_text(doc, 10)
  
 # now print the results
 print("\n=====Doc=====")
-print(doc)
+print(title)
 print("\n===Keywords===")
 for k in keywords:
     print(k,keywords[k])
+
 
 # %% [markdown]
 # ## Try some matching
 
+# %% [markdown]
+# ### Some functions to calculate scores
+
+# %%
+#First try: for each doc take the 10 first words, and scores, and if there are the sames as the choosen articles,
+# multiply there scores and add to score
+def get_scores_simple(articles, target_article_keywords, nb_vectors):
+    scores = []
+    for article in articles:
+        score = 0
+        article_kws = get_kekwords_from_text(article, nb_vectors)
+        for kword in target_article_keywords:
+            for kw in article_kws:
+                if kword == kw:
+                    score += 1 - target_article_keywords[kword]*article_kws[kw]
+                else:
+                    score += 1
+        scores.append(score)
+    return(scores)
+
+
+# %%
+#Second try, some euclidian distance
+def get_scores_with_euclidian_distance(articles, target_article_keywords, nb_vectors):
+    scores = []
+    for article in articles:
+        score = 0
+        article_kws = get_kekwords_from_text(article, nb_vectors)
+        for kword in target_article_keywords:
+            for kw in article_kws:
+                if kword == kw:
+                    score += (target_article_keywords[kword]-article_kws[kw])**2
+                else:
+                    score += 1
+        scores.append(score)
+    return(scores)
+
+
+# %% [markdown]
+# ### Choose and preprocess an article
+
 # %%
 #Choose an article
-file3 = open('cleaned_articles/1999/29_07_1999.json', 'r') # Juifs marocains
-read_file3 = json.loads(file3.read())[0]
-title3 = read_file3['title']
-text3 = read_file3['text']
+articleNb=146
+target_article=processed_articles[articleNb]
+title=titles[articleNb]
 
-#generate tf-idf for the given document
-tf_idf_vector=tfidf_transformer.transform(cv.transform([text3]))
- 
-#sort the tf-idf vectors by descending order of scores
-sorted_items=sort_coo(tf_idf_vector.tocoo())
- 
-#extract only the top n; n here is 10
-keywords=extract_topn_from_vector(feature_names,sorted_items,10)
+nb_vectors = 10
+
+target_article_keywords = get_kekwords_from_text(target_article, nb_vectors)
  
 # now print the results
 print("\n=====Doc=====")
-print(title3)
+print(title)
 print("\n===Keywords===")
-for k in keywords:
-    print(k,keywords[k])
+for k in target_article_keywords:
+    print(k,target_article_keywords[k])
+
+
+# %% [markdown]
+# ### Calculate scores related to our choosen article
 
 # %%
-#Look which article matches the best for this kwords ?
-scores = []
-for article in articles:
-    score = 0
-    tf_idf_vector=tfidf_transformer.transform(cv.transform([article]))
-    sorted_items=sort_coo(tf_idf_vector.tocoo())
-    kws=extract_topn_from_vector(feature_names,sorted_items,10)
-    for kword in keywords:
-        for kw in kws:
-            if kword == kw:
-                score += keywords[kword]*kws[kw]
-    scores.append(score)
-print(scores)
+print('...')
+scores = get_scores_with_euclidian_distance(processed_articles, target_article_keywords, nb_vectors)
+print('Done')
+
+# %% [markdown]
+# ### Find matches
 
 # %%
+nb_of_match_to_dislay = 3
+results = scores[:]
+
+# %%
+for i in range(nb_of_match_to_dislay):
+    index = scores.index(min(results))
+    matched_article = processed_articles[index]
+    article_kws = get_kekwords_from_text(matched_article, nb_vectors)
+    print('\n Titre: ',titles[index])
+    print("\n===Keywords===")
+    for k in article_kws:
+        print(k,article_kws[k])
+    results[index]=100
+
+# %% [raw]
+# Les résultats restent très approximatifs, le matching ne donne pas de bons résultats.
+# Améliorations à penser:
+# - changer manière de calculer les scores (dstance euclidienne ? sur quoi ?)
+# - tester avec plus d'articles
+# - mélanger avec topic modelling
