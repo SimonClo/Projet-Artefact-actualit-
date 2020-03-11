42adc293df8550f3fb5dfb4335f7f3e15732bf9e
AnneLaurene <annelaurene.harmel@free.fr>
2020-03-11 14:20:47 +0100

    seed topics in pipeline

---
+++ modelling/force_topics.py
@@ -0,0 +1,41 @@
+# Create the topics
+import numpy as np
+
+
+# function to enter the topic in eta
+def add_topic_to_eta(words, topic_nb, eta):
+    print('Topic number '+str(topic_nb))
+    for word in words:
+        if word in dictionary2.token2id:
+            eta[topic_nb, dictionary2.token2id[word]]*=10
+        else:
+            print(word,' not in the dictionary')
+
+# Functions that returns an eta matrix, depending on the n
+def create_eta(topics, num_topics, dictionary):
+    eta = np.ones((num_topics, len(dictionary)))*1/num_topics
+    if num_topics < len(topics):
+        print('You try to seed to many topics')
+        return None
+    else:
+        topic_nb = 0
+        for topic in topics:
+            add_topic_to_eta(topic, topic_nb, eta)
+            topic_nb += 1
+    return eta
+
+# special topics
+words_sport = ['sport', 'olymp', 'athlet', 'défait', 'victoir', 'match', 'foot', 'champion', 'football', 'basket', 'roland', 'médaill', 'club', 'jeux', 'supporter'] # marche, associé à la tété chaine, cavle etc ...
+words_ecology = ['durabl', 'planet', 'vert', 'ecolo', 'carbon', 'climat', 'écolog', 'énerg', 'nucléair', 'serr', 'gaz', 'réchauff']
+words_school = ['scolair','écol','enseignement', 'enseign','étud','lycéen','orient','enfant', 'réform']
+words_religion= ['églis', 'eglis', 'prêtr', 'vatican', 'relig', 'mess', 'évêqu', 'chrétien', 'catholic', 'catholique', 'cathol', 'abbé']
+words_space = ['espac', 'astronaut', 'navet', 'spatial', 'lunair', 'satellit', 'apollo', 'terr', 'solair', 'orbit', 'cabin', 'oxygen']
+words_shoah = ['juif', 'extermin', 'auschwitz', 'antisémit', 'camp', 'hitler', 'nazism', 'adolf', 'ghetto', 'rabbin', 'wagon', 'reich']
+words_music = ['musiqu', 'concert', 'chanson', 'album']
+words_seism = ['séism', 'humanitair', 'mort', 'trembl', 'terr']
+# film ? télé ? reseaux sociaux ?
+
+topics = [words_ecology, words_sport, words_school, words_religion, words_space, words_shoah, words_music, words_seism]
+
+def get_eta(num_topics, dictionary):
+    return create_eta(topics, num_topics, dictionary)
