d2bca6b03766793b3809f4b1489d0fa9e8fa5e60
Antoine-marchais <antoine.marchais@student.ecp.fr>
2020-04-07 21:11:23 +0200

    dockerize backoffice

--- backoffice/config.py
+++ backoffice/config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:8765/artefact_archives"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@database:5432/artefact_archives"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
